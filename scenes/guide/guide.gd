783a0879f039b711a91beeda6740bb87347cc981
Gabe-Manaog <alonzogabriel.manaog@ontariotechu.net>
2025-02-24 14:08:26 -0500

    Made a Dialogue for NPC

--- scenes/guide/guide.gd
+++ scenes/guide/guide.gd
@@ -2,6 +2,7 @@ extends Node2D
 var balloon_scene = preload("res://dialogue/game_dialogue_balloon.tscn")
 @onready var interactable_component: InteractableConponent = $InteractableComponent
 @onready var interactable_label_component: Control = $InteractableLabelComponent
+var in_range: bool
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interactable_component.interactable_activated.connect(on_interactable_activated)
@@ -9,11 +10,13 @@ func _ready() -> void:
 	interactable_label_component.hide()
 func on_interactable_activated()-> void:
 	interactable_label_component.show()
+	in_range=true
 func on_interactable_deactivated()->void:
 	interactable_label_component.hide()
+	in_range=false
 func _unhandled_input(event: InputEvent) -> void:
-#	if event.is_action_pressed("show_dialogue"):
-#		var balloon: BaseGameDialoagueBalloon = balloon_scene.instantiate()
-#		get_tree().current_scence.add_child(balloon)
-#		balloon.start(load("res://dialogue/conversations/guide.dialogue","start"))
-	pass
+	if in_range:	
+		if event.is_action_pressed("show_dialogue"):
+			var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate()
+			get_tree().current_scene.add_child(balloon)
+			balloon.start(load("res://dialogue/conversations/guide.dialogue"),"start")
