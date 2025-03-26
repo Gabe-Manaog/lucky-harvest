13ef6470662f003b8517a0ace12f3deec630d5b4
MilanHennessy <milanhennessy@gmail.com>
2025-03-26 14:24:09 -0400

    changed proc gen

--- scripts/worlds/test_proc_gen_world.gd
+++ scripts/worlds/test_proc_gen_world.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@onready var noise_generator: NoiseGenerator = $NoiseGenerator
-const SMALL_TREE = preload("res://scenes/trees/small_tree.tscn")
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
