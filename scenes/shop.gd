13919cf0a315a12c0d863f9b146b76c56a03c9a6
Gabe-Manaog <alonzogabriel.manaog@ontariotechu.net>
2025-03-27 18:01:28 -0400

    You can move next in Shop

--- scenes/shop.gd
+++ scenes/shop.gd
@@ -2,12 +2,27 @@ extends CanvasLayer
 
 @onready var currency_label: Label = $Panel/Control/CurrencyPanel/MarginContainer/CurrencyLabel
 var currency = 0
+var current_page = 0
 var inventory = InventoryManager.inventory
+var crops_dict = {
+	0:{"name":"Corn",
+"description": "Long Yellow Snack",
+"cost": 500
+},
+	1:{"name":"Tomato",
+"description": "Delectable Red Treat",
+"cost": 500
+},
+	2:{"name":"Potato",
+"description": "Potatoe potahto",
+"cost": 700
+},
+}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsed time since thce previous frame.
 func _process(delta: float) -> void:
 	pass
 
@@ -17,13 +32,26 @@ func _on_close_pressed() -> void:
 	get_tree().paused = false
 
 func switchItem(select):
-	print(InventoryManager.inventory[select])
+	for i in range(crops_dict.size()):
+		if i == current_page:
+			print(current_page)
+			current_page=select
+			get_node("Panel/Control/Icon/AnimatedSprite2D").play(crops_dict[current_page]["name"])
+			get_node("Panel/Control/Name").text=(crops_dict[current_page]["name"])
+			get_node("Panel/Control/Description").text=(crops_dict[current_page]["description"])
+			get_node("Panel/Control/Description").text+=("\n Cost: "+ str(crops_dict[current_page]["cost"]))
 
+
+
+			
 func _on_next_pressed() -> void:
-	print(inventory)
+	if current_page<crops_dict.size()-1:
+		switchItem(current_page+1)
 
 func _on_previous_pressed() -> void:
-	pass
+	if current_page>0:
+		switchItem(current_page-1)
+
 
 
 func _on_purchase_pressed() -> void:
