5e89ed3445f7f6665b04bd91f9ddbee967e34e02
MilanHennessy <milanhennessy@gmail.com>
2025-03-29 12:10:45 -0400

    made energy bar look nicer

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -3,12 +3,15 @@ extends CharacterBody2D
 
 @onready var hit_component: HitComponent = $HitComponent
 @onready var inventory_panel: PanelContainer = $InventoryPanel
-@onready var energy_bar: ProgressBar = $EnergyBar
+@onready var energy_bar_ui: Control = $EnergyBar  # The EnergyBar node inside Player scene
+
 
 
 @export var current_tool: DataTypes.Tools = DataTypes.Tools.None
 var player_direction: Vector2
 var energy
+
+var energy_bar = energy_bar_ui.get_node("ProgressBar") as ProgressBar
 
 func _ready() -> void:
 	ToolManager.tool_selected.connect(on_tool_selected)
