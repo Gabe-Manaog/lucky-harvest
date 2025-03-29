2e8071d749d74c758a630a884f02a9eb6c2f0936
MilanHennessy <milanhennessy@gmail.com>
2025-03-29 13:15:32 -0400

    made energy bar look good

--- scenes/worlds/main/world_one.gd
+++ scenes/worlds/main/world_one.gd
@@ -8,7 +8,8 @@ var energy
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	energy_bar.value = 100
+	pass
+#	energy_bar.value = 100
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
