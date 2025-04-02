6b87c5d6c5ebf13fa54ede02aa535c35a373260d
MilanHennessy <milanhennessy@gmail.com>
2025-04-02 11:05:49 -0400

    added de-selecting tools

--- scripts/Ui/tool_panel.gd
+++ scripts/Ui/tool_panel.gd
@@ -3,7 +3,9 @@ extends PanelContainer
 @onready var tool_axe: Button = $MarginContainer/HBoxContainer/Tool_Axe
 @onready var tool_tilling: Button = $MarginContainer/HBoxContainer/Tool_Tilling
 @onready var tool_water: Button = $MarginContainer/HBoxContainer/Tool_Water
-@onready var tool_plant: Button = $MarginContainer/HBoxContainer/Tool_Plant
+@onready var tool_corn: Button = $MarginContainer/HBoxContainer/Tool_Corn
+@onready var tool_tomato: Button = $MarginContainer/HBoxContainer/Tool_Tomato
+
 @onready var tool_potato: Button = $MarginContainer/HBoxContainer/Tool_Potato
 @onready var player: Player = $".."
 @onready var input_blocker: ColorRect = $InputBlocker
@@ -11,14 +13,13 @@ extends PanelContainer
 
 
 
-#func _unhandled_input(event: InputEvent) -> void:
-	#if event.is_action_pressed("release_tool"):
-		#ToolManager.select_tool(DataTypes.Tools.None)
-		#tool_axe.release_focus()
-		#tool_tilling.release_focus()
-		#tool_water.release_focus()
-		#tool_plant.release_focus()
-
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("release_tool"):
+		ToolManager.select_tool(DataTypes.Tools.None)
+		tool_axe.release_focus()
+		tool_tilling.release_focus()
+		tool_water.release_focus()
+		
 func _on_tool_axe_pressed() -> void:
 	player.current_tool = DataTypes.Tools.None
 	ToolManager.select_tool(DataTypes.Tools.AxeWood)
